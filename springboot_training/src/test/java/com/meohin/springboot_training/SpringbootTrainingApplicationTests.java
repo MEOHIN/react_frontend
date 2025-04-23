6c2218128d66053e70ca2f40fd7ec4ec0b520ee5
meohin <meohin.meo@gmail.com>
2025-04-23 21:29:44 +0900

    First commit

---
+++ springboot_training/src/test/java/com/meohin/springboot_training/SpringbootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meohin.springboot_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
