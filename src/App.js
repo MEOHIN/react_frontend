0c9c7e3ae2927349c8fb634685fbf5c897ebb64c
meohin <meohin.meo@gmail.com>
2025-04-28 17:02:41 +0900

    feat: App.js에 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import PostDetail from "./pages/PostDetail";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
